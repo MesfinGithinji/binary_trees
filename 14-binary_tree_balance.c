26cc4917986300266444c9150adee21357c25c82
MesfinGithinji <meshgithinji@gmail.com>
2023-08-03 17:37:46 +0300

    Function that measures the balance factor of a binary tree

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,30 +1,27 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_height_new - measure a binary tree's height
+ * binary_tree_height - measure a binary tree's height
  * @tree: tree to measure the height of
  *
  * Return: height of the tree
  *         0 if tree is NULL
  */
-size_t binary_tree_height_new(const binary_tree_t *tree)
+size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left_side = 0;
-	size_t rightside = 0;
+	size_t height_left = 0;
+	size_t height_right = 0;
 
 	if (tree == NULL)
-	{
 		return (0);
-	}
+
+	height_left += 1 + binary_tree_height(tree->left);
+	height_right += 1 + binary_tree_height(tree->right);
+
+	if (height_left > height_left)
+		return (height_left);
 	else
-	{
-		if (tree)
-		{
-			left_side = tree->left ? 1 + binary_tree_height_new(tree->left) : 1;
-			right_side = tree->right ? 1 + binary_tree_height_new(tree->right) : 1;
-		}
-		return ((left_side > right_side) ? left_side : right_side);
-	}
+		return (height_left);
 }
 
 /**
@@ -39,8 +36,8 @@ int binary_tree_balance(const binary_tree_t *tree)
 
 	if (tree)
 	{
-		left_side = ((int)binary_tree_height_new(tree->left));
-		right_side = ((int)binary_tree_height_new(tree->right));
+		left_side = ((int)binary_tree_height(tree->left));
+		right_side = ((int)binary_tree_height(tree->right));
 		balance = left_side - right_side;
 	}
 	return (balance);
